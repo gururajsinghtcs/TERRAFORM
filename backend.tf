619209aac935f03d74ce2ff080a392de87eb698e
gururajsinghtcs <113055196+gururajsinghtcs@users.noreply.github.com>
2022-09-09 21:40:07 +0530

    Create backend.tf
    This files contains backend of every project startup , include region , dynamo db for lock and S3 bucket to keep state file , so every one can see changes.

---
+++ backend.tf
@@ -0,0 +1,34 @@
+
+# Provider Details
+provider "aws" {
+  region                   = "ap-south-1"
+  shared_credentials_files = ["C:\\Users\\gurur\\.aws\\credentials"]
+}
+
+
+
+# Create S3 Bucket and DynamoDB table to store state and state lock.
+resource "aws_s3_bucket" "bucket" {
+    bucket = "guru24bucket-t"
+    object_lock_configuration {
+        object_lock_enabled = "Enabled"
+    }
+    tags = { 
+        Name = "S3 Remote Terraform State Store"
+    }
+}
+\
+
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "dpt4_table"
+    read_capacity  = 5 
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
+}
