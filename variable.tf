42e800df344625f6477c0d51f3e1a944374258eb
gururajsinghtcs <113055196+gururajsinghtcs@users.noreply.github.com>
2022-09-09 21:45:21 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "subnet_az" {
+    type =string
+}
+variable "region"  {
+    type =string
+}
+variable "subnet_tag" {
+    type =string
+} 
+variable "subnet_cidr"  {
+    type =string
+}
