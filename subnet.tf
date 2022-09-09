d58d2e17db34235ab9e75ca8b90c232a571c9a4d
gururajsinghtcs <113055196+gururajsinghtcs@users.noreply.github.com>
2022-09-09 21:54:21 +0530

    Delete subnet.tf

--- subnet.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.myvpc.id
-  availability_zone = var.subnet_az
-  cidr_block = var.subnet_cidr
-  tags = {
-    "key" = var.subnet_tag
-  }
-}
