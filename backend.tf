7b97c67292190306b1e256e5574862c2c857c89e
gururajsinghtcs <113055196+gururajsinghtcs@users.noreply.github.com>
2022-10-10 12:08:59 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,9 @@
+# # Provider Details
+# provider "aws" {
+#   region                   = "ap-south-1"
+#   shared_credentials_files = ["C:\\Users\\gurur\\.aws\\credentials"]
+# }
+# optional code for--- aws configure  ---command to avoid every time so put file at this location  C:\\Users\\gurur\\.aws\\credentials  userid secret key
 
 
 # backend block- s3 Dynamodb
